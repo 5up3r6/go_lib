d4d4a8213a7e21c506f0d97a5df2bc627fa86c44
w1z4rd <52351361+5up3r6@users.noreply.github.com>
2024-11-24 11:13:59 +0800

    Add files via upload

---
+++ go_lib.go
@@ -0,0 +1,19 @@
+package go_lib_test
+
+import "fmt"
+
+var HelloWorld bool
+
+func init() {
+	fmt.Println("Hello, World! init")
+}
+
+// SayHello 打印 Hello, World!
+func SayHello() {
+	fmt.Println("Hello, World!")
+}
+
+// SayGreeting 接受一个名字并打印问候语
+func SayGreeting(name string) {
+	fmt.Printf("Hello, %s!\n", name)
+}
